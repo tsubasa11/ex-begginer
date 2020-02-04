9fdd8221411c16e65a1eeeaa219767068b523652
tsubasa <tsubasa.namiki@rakus-partners.co.jp>
2020-02-04 14:50:17 +0900

    計算結果画面exam02-result.htmlより画面遷移後のexam02-result2.html作成(sessionスコープ)

--- src/main/java/ex/example/Exam02Controller.java
+++ src/main/java/ex/example/Exam02Controller.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * exam02.htmlのcontroller
+ * 
  * @author namikitsubasa
  *
  */
@@ -18,18 +19,24 @@ public class Exam02Controller {
 
 	@Autowired
 	private HttpSession session;
-	
+
 	@RequestMapping("")
 	public String index() {
-		
+
 		return "exam02";
 	}
-	
-	@RequestMapping("/result")	
-	public String result(Integer num1,Integer num2) {
+
+	@RequestMapping("/result")
+	public String result(Integer num1, Integer num2) {
 		session.setAttribute("num1", num1);
 		session.setAttribute("num2", num2);
-		session.setAttribute("result", num1+num2);
+		session.setAttribute("result", num1 + num2);
+		return "exam02-result";
+	}
+
+	@RequestMapping("/result2")
+	public String result2() {
+
 		return "exam02-result2";
 	}
 }
